98239226943427670cd837a544bd7015c4e2680a
philippe SIMO <philippe.guemkamsimo@gmail.com>
2021-12-19 09:48:37 +0100

    Configure cors according to the running env

---
+++ src/main/java/tech/zerofiltre/blog/infra/entrypoints/rest/config/WebConfiguration.java
@@ -0,0 +1,22 @@
+package tech.zerofiltre.blog.infra.entrypoints.rest.config;
+
+import lombok.extern.slf4j.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.context.annotation.*;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+@EnableWebMvc
+@Slf4j
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Value("${zerofiltre.infra.entrypoints.rest.allowed-origins-pattern}")
+    private String allowedOriginsPattern;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        log.debug("Registering this allowed origins pattern: {}", allowedOriginsPattern);
+        registry.addMapping("/**")
+                .allowedOriginPatterns(allowedOriginsPattern);
+    }
+}
