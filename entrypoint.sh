059c47632ebf4f53fbc52499ee7200ef53ec278d
geek3000 <jonathantchuente@gmail.com>
2022-08-04 06:45:48 +0100

    change vault call

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 echo "The app is starting ..."
-source /vault/secrets/config
+
+chmod +x /vault/secrets/config
+
+. /vault/secrets/config
+
 java -jar -Xms1g -Xmx4g -XX:+ExitOnOutOfMemoryError -Dspring.profiles.active=kubernetes "blog.jar"
