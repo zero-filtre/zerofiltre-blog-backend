e81c2201fc238ec37c0a6e53ea386b540b02954f
geek3000 <58487765+geek3000@users.noreply.github.com>
2022-08-10 23:26:16 +0100

    230 secret injection (#7)
    Use vault to inject secrets

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 echo "The app is starting ..."
-exec java -jar -Xms1g -Xmx4g -XX:+ExitOnOutOfMemoryError -Dspring.profiles.active=kubernetes "blog.jar"
+
+source /vault/secrets/config
+
+java -jar -Xms1g -Xmx4g -XX:+ExitOnOutOfMemoryError -Dspring.profiles.active=kubernetes "blog.jar"
