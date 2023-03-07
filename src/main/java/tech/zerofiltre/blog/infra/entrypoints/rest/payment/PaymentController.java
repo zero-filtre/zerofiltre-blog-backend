fcc044b73172defe7bb08497c7925b48bd9d2cfc
Philippe GUEMKAM SIMO <philippechampion58@gmail.com>
2023-03-07 13:53:55 +0100

    Create stripe customer during checkout if not present

--- src/main/java/tech/zerofiltre/blog/infra/entrypoints/rest/payment/PaymentController.java
+++ src/main/java/tech/zerofiltre/blog/infra/entrypoints/rest/payment/PaymentController.java
@@ -5,6 +5,7 @@ import com.stripe.exception.*;
 import com.stripe.model.*;
 import com.stripe.model.checkout.*;
 import com.stripe.net.*;
+import com.stripe.param.*;
 import com.stripe.param.checkout.*;
 import lombok.extern.slf4j.*;
 import org.springframework.web.bind.annotation.*;
@@ -59,67 +60,26 @@ public class PaymentController {
     @PostMapping("/checkout")
     public String createCheckoutSession(@RequestBody @Valid ChargeRequestVM chargeRequestVM) throws PaymentException, ResourceNotFoundException {
         User user = securityContextManager.getAuthenticatedUser();
-
         Product product = getProduct(chargeRequestVM.getProductId(), chargeRequestVM.getProductType());
 
+        try {
 
-        SessionCreateParams.LineItem.PriceData.ProductData productData = SessionCreateParams.LineItem.PriceData.ProductData.builder()
-                .putMetadata(PRODUCT_ID, String.valueOf(product.getId()))
-                .putMetadata(USER_ID, String.valueOf(user.getId()))
-                .setName(product.getTitle())
-                .setDescription(product.getSummary())
-                .addImage(product.getThumbnail() == null || product.getThumbnail().isBlank() ?
-                        "https://ik.imagekit.io/lfegvix1p/tr:w-800,ar-auto,dpr-auto,di-Article_default_oz_Yb-VZj.svg/not_found_image.jpg"
-                        : product.getThumbnail())
-                .build();
-
-        SessionCreateParams.LineItem.PriceData.Builder priceDataBuilder = SessionCreateParams.LineItem.PriceData.builder()
-                .setCurrency(ChargeRequest.Currency.EUR.getValue())
-                .setTaxBehavior(SessionCreateParams.LineItem.PriceData.TaxBehavior.INCLUSIVE)
-                .setUnitAmount(product.getPrice())
-                .setProductData(productData);
-
-        SessionCreateParams.Mode mode = SessionCreateParams.Mode.valueOf(chargeRequestVM.getMode().toUpperCase());
-
-        if (mode.equals(SessionCreateParams.Mode.SUBSCRIPTION)) {
-            SessionCreateParams.LineItem.PriceData.Recurring recurring = SessionCreateParams.LineItem.PriceData.Recurring.builder()
-                    .setInterval(SessionCreateParams.LineItem.PriceData.Recurring.Interval.valueOf(chargeRequestVM.getRecurringInterval().toUpperCase()))
+            CustomerSearchParams customerSearchParams = CustomerSearchParams.builder()
+                    .setQuery("metadata['" + USER_ID + "']:'" + user.getId() + "'")
                     .build();
 
-            priceDataBuilder.setRecurring(recurring);
-        }
-
-        SessionCreateParams.LineItem lineItem = SessionCreateParams.LineItem.builder()
-                .setQuantity(1L)
-                .setPriceData(priceDataBuilder.build())
-                .build();
-
-        SessionCreateParams.AutomaticTax automaticTax = SessionCreateParams.AutomaticTax.builder()
-                .setEnabled(true)
-                .build();
-
-        String originURL = ZerofiltreUtils.getOriginUrl(infraProperties.getEnv());
-        SessionCreateParams.Builder sessionBuilder = SessionCreateParams.builder()
-                .setMode(mode)
-                .setSuccessUrl(originURL + "/payment/success")
-                .setCancelUrl(originURL + "/payment/cancel")
-                .setLocale(SessionCreateParams.Locale.FR)
-                .setAutomaticTax(automaticTax)
-                .addLineItem(lineItem);
-
-
-        if (EmailValidator.validateEmail(user.getEmail()))
-            sessionBuilder.setCustomerEmail(user.getEmail());
-
-        SessionCreateParams sessionCreateParams = sessionBuilder.build();
-
-        try {
-            Session session = Session.create(sessionCreateParams);
-            return session.getUrl();
+            CustomerSearchResult result = Customer.search(customerSearchParams);
+            if (!result.getData().isEmpty()) {
+                Customer customer = result.getData().get(0);
+                log.info("Customer for user {} found on stripe, using him to create checkout session.: {}", user.getEmail(), customer.toString().replace("\n", " "));
+                return createSession(chargeRequestVM, product, customer);
+            }
+            return createSession(chargeRequestVM, product, createCustomer(user));
         } catch (StripeException e) {
             log.error("Error while initializing the checkout session: " + e.getLocalizedMessage(), e);
             throw new PaymentException("Error while initializing the checkout session" + e.getLocalizedMessage(), "");
         }
+
     }
 
 
@@ -154,12 +114,15 @@ public class PaymentController {
 
             SessionListLineItemsParams listLineItemsParams = SessionListLineItemsParams.builder()
                     .addExpand("data.price.product")
+                    .addExpand("data.customer")
                     .build();
 
             LineItemCollection lineItems = session.listLineItems(listLineItemsParams);
-            Session.CustomerDetails customerDetails = session.getCustomerDetails();
-            log.info("Customer email: {}", customerDetails != null ? customerDetails.getEmail() : "no customer details provided");
-
+//            Session.CustomerDetails customerDetails = session.getCustomerDetails();
+//            log.info("Customer email: {}", customerDetails != null ? customerDetails.getEmail() : "no customer details provided");
+            Customer customer = session.getCustomerObject();
+            log.info("Customer: {}", customer != null ? customer.toString().replace("\n", " ") : "no customer provided");
+            log.info("User id: {}", customer != null ? customer.getMetadata().get(USER_ID) : "no customer id provided");
 
             for (LineItem lineItem : lineItems.getData()) {
                 log.info("Line item: {}", lineItem.toString().replace("\n", " "));
@@ -194,4 +157,92 @@ public class PaymentController {
         }
     }
 
+
+    private Customer createCustomer(User user) throws StripeException {
+
+        CustomerCreateParams.Builder customerParamsBuilder = CustomerCreateParams.builder()
+                .setName(user.getFullName());
+
+        if (EmailValidator.validateEmail(user.getEmail()))
+            customerParamsBuilder.setEmail(user.getEmail());
+
+        customerParamsBuilder.putMetadata(USER_ID, String.valueOf(user.getId()));
+        Customer customer = Customer.create(customerParamsBuilder.build());
+        log.info("Customer created on stripe, he will be used to create the session: {}", customer.toString().replace("\n", " "));
+        return customer;
+    }
+
+    private String createSession(ChargeRequestVM chargeRequestVM, Product product, Customer customer) throws StripeException {
+
+
+        SessionCreateParams.LineItem.PriceData.ProductData productData = SessionCreateParams.LineItem.PriceData.ProductData.builder()
+                .putMetadata(PRODUCT_ID, String.valueOf(product.getId()))
+                .setName(product.getTitle())
+                .setDescription(product.getSummary())
+                .addImage(product.getThumbnail() == null || product.getThumbnail().isBlank() ?
+                        "https://ik.imagekit.io/lfegvix1p/tr:w-800,ar-auto,dpr-auto,di-Article_default_oz_Yb-VZj.svg/not_found_image.jpg"
+                        : product.getThumbnail())
+                .build();
+
+        SessionCreateParams.LineItem.PriceData.Builder priceDataBuilder = SessionCreateParams.LineItem.PriceData.builder()
+                .setCurrency(ChargeRequest.Currency.EUR.getValue())
+                .setTaxBehavior(SessionCreateParams.LineItem.PriceData.TaxBehavior.INCLUSIVE)
+                .setUnitAmount(product.getPrice())
+                .setProductData(productData);
+
+        SessionCreateParams.Mode mode = SessionCreateParams.Mode.valueOf(chargeRequestVM.getMode().toUpperCase());
+
+
+        if (mode.equals(SessionCreateParams.Mode.SUBSCRIPTION)) {
+            SessionCreateParams.LineItem.PriceData.Recurring recurring = SessionCreateParams.LineItem.PriceData.Recurring.builder()
+                    .setInterval(SessionCreateParams.LineItem.PriceData.Recurring.Interval.valueOf(chargeRequestVM.getRecurringInterval().toUpperCase()))
+                    .build();
+
+            priceDataBuilder
+                    .setUnitAmount(product.getPrice())
+                    .setRecurring(recurring);
+
+            //make sure to set the right price depending on payment mode
+            priceDataBuilder.setUnitAmount(product.getPrice());
+        } else if (mode.equals(SessionCreateParams.Mode.SETUP)) {
+
+
+            //make sure to set the right price depending on payment mode
+            priceDataBuilder.setUnitAmount(product.getPrice());
+        }
+
+
+        SessionCreateParams.LineItem lineItem = SessionCreateParams.LineItem.builder()
+                .setQuantity(1L)
+                .setPriceData(priceDataBuilder.build())
+                .build();
+
+        SessionCreateParams.AutomaticTax automaticTax = SessionCreateParams.AutomaticTax.builder()
+                .setEnabled(true)
+                .build();
+
+        SessionCreateParams.CustomerUpdate customerUpdate = SessionCreateParams.CustomerUpdate.builder()
+                .setAddress(SessionCreateParams.CustomerUpdate.Address.AUTO)
+                .build();
+
+        String originURL = ZerofiltreUtils.getOriginUrl(infraProperties.getEnv());
+        SessionCreateParams.Builder sessionBuilder = SessionCreateParams.builder()
+                .setMode(mode)
+                .setSuccessUrl(originURL + "/payment/success")
+                .setCancelUrl(originURL + "/payment/cancel")
+                .setCustomer(customer.getId())
+                .setCustomerUpdate(customerUpdate)
+                .setLocale(SessionCreateParams.Locale.FR)
+                .setAutomaticTax(automaticTax)
+                .addLineItem(lineItem);
+
+
+        SessionCreateParams sessionCreateParams = sessionBuilder.build();
+        Session session = Session.create(sessionCreateParams);
+        return session.getUrl();
+
+    }
+
 }
+
+
