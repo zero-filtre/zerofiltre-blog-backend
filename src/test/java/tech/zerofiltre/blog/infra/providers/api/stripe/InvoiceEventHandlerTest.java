ddad2be35ff9379057d3e0ca5937c9855ec42f59
hotiaDiallo <ibrahimahotia.diallo@gmail.com>
2024-01-30 09:35:15 +0100

    WIP TU for Cancel 3 Times Payment

--- src/test/java/tech/zerofiltre/blog/infra/providers/api/stripe/InvoiceEventHandlerTest.java
+++ src/test/java/tech/zerofiltre/blog/infra/providers/api/stripe/InvoiceEventHandlerTest.java
@@ -2,13 +2,12 @@ package tech.zerofiltre.blog.infra.providers.api.stripe;
 
 import com.stripe.exception.StripeException;
 import com.stripe.model.Subscription;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import tech.zerofiltre.blog.domain.error.ZerofiltreException;
 
-import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class InvoiceEventHandlerTest {
@@ -16,19 +15,20 @@ class InvoiceEventHandlerTest {
     @Mock
     InvoiceEventHandler invoiceEventHandler;
 
-    @Test
-    void should_cancel_if_mentored() throws StripeException, ZerofiltreException {
-        Subscription subscription = mock(Subscription.class);
-
-
-        //
-
-        //act
-        invoiceEventHandler.cancelFor3TimesPayment(null, null, false, subscription, 3, null);
-
-        //assert
-        verify(subscription, times(0)).cancel();
-
-    }
+    //    @Test
+//    @Disabled("To be implemented by Ibrahima")
+//    void should_cancel_if_mentored() throws StripeException {
+//        Subscription subscription = mock(Subscription.class);
+//
+//
+//        //
+//
+//        //act
+//        invoiceEventHandler.cancelFor3TimesPayment(null, null, false, subscription, 3, null);
+//
+//        //assert
+//        verify(subscription, times(0)).cancel();
+//
+//    }
 
 }
