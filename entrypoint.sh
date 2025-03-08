c73bd883bb596adb1da2606401e8988b275e46c8
Philippe GUEMKAM <philippe_guemkam@ext.carrefour.com>
2025-03-08 12:05:43 +0100

    Increase mem limit for prod and dump heap if OOM

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ echo "The app is starting ..."
 
 source /vault/secrets/config
 
-java -jar -Xms1g -Xmx4g -XX:+ExitOnOutOfMemoryError -javaagent:opentelemetry-javaagent.jar -Dspring.profiles.active=kubernetes -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 "blog.jar"
+java -jar -Xms1g -Xmx4g -XX:+ExitOnOutOfMemoryError -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/heapdumps -javaagent:opentelemetry-javaagent.jar -Dspring.profiles.active=kubernetes -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 "blog.jar"
