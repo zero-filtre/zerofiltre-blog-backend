98886f9ca7e3bb70cb835285edda34e9e4b2ea16
Philippe PERNET <philip.pernet@free.fr>
2025-05-14 14:41:53 +0200

    [820] company admin can add user with admin role

--- src/main/java/tech/zerofiltre/blog/util/DataChecker.java
+++ src/main/java/tech/zerofiltre/blog/util/DataChecker.java
@@ -129,19 +129,6 @@ public class DataChecker {
         return true;
     }
 
-    public boolean hasPermission(User connectedUser, long companyId, LinkCompanyUser.Role role) throws ForbiddenActionException {
-        if (role.equals(LinkCompanyUser.Role.ADMIN)
-                && !connectedUser.isAdmin()) {
-            throw new ForbiddenActionException("You don't have authorization.");
-        } else if ((role.equals(LinkCompanyUser.Role.EDITOR)
-                || role.equals(LinkCompanyUser.Role.VIEWER))
-                && !connectedUser.isAdmin()
-                && !isCompanyAdmin(connectedUser.getId(), companyId)) {
-            throw new ForbiddenActionException("You don't have authorization.");
-        }
-        return true;
-    }
-
     boolean isCompanyAdmin(long userId, long companyId) {
         Optional<LinkCompanyUser> companyUser = companyUserProvider.findByCompanyIdAndUserId(companyId, userId, true);
 
