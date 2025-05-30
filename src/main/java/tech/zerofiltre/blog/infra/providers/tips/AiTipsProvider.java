5bd57d4bf31fb83fc954cca83ccef9132079d5ab
Philippe GUEMKAM <philippe_guemkam@ext.carrefour.com>
2025-05-30 15:55:38 +0200

    713 Enhance tip of the day

--- src/main/java/tech/zerofiltre/blog/infra/providers/tips/AiTipsProvider.java
+++ src/main/java/tech/zerofiltre/blog/infra/providers/tips/AiTipsProvider.java
@@ -1,14 +1,18 @@
 package tech.zerofiltre.blog.infra.providers.tips;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.RandomUtils;
 import org.springframework.stereotype.Component;
 import tech.zerofiltre.blog.domain.error.ZerofiltreException;
 import tech.zerofiltre.blog.domain.logging.MessageSourceProvider;
 import tech.zerofiltre.blog.domain.tips.AiProvider;
 import tech.zerofiltre.blog.domain.tips.TipsProvider;
-import tech.zerofiltre.blog.domain.tips.model.Tip;
+import tech.zerofiltre.blog.infra.InfraProperties;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 @Component
@@ -17,24 +21,30 @@ public class AiTipsProvider implements TipsProvider {
 
     private final MessageSourceProvider messageSourceProvider;
     private final AiProvider aiProvider;
+    private final InfraProperties infraProperties;
+
+    @Getter
+    private String tip;
+    @Getter
+    private LocalDateTime date;
 
     @Override
     public String getTip(String profession, Locale locale) throws ZerofiltreException {
-        String request  = messageSourceProvider.getMessage("message.tip.ai.prompt", null, locale);
-
-        if(profession.isEmpty()) {
-            return findTip(request + messageSourceProvider.getMessage("message.tip.ai.prompt.profession", null, locale));
+        List<String> prompts = new ArrayList<>();
+        for (int i = 0; i <= 20; i++) {
+            prompts.add(messageSourceProvider.getMessage("message.tip.ai.prompt_" + i, null, locale));
         }
-
-        return aiProvider.answer(request + profession);
+        int promptIndex = RandomUtils.nextInt(0, prompts.size());
+        String request = prompts.get(promptIndex);
+        return findTip(request);
     }
 
     String findTip(String request) throws ZerofiltreException {
-        if(Tip.date == null || !Tip.date.equals(LocalDate.now()) || Tip.tip == null || Tip.tip.isBlank()) {
-            Tip.tip = aiProvider.answer(request);
-            Tip.date = LocalDate.now();
-        }
-        return Tip.tip;
+        if (date != null && date.isAfter(LocalDateTime.now().minusSeconds(infraProperties.getRefreshInterval())) && tip != null && !tip.isBlank())
+            return tip;
+        tip = aiProvider.answer(request);
+        date = LocalDateTime.now();
+        return tip;
     }
 
 }
